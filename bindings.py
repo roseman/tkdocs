d26a605c9c05ea2bc40bbcf0d683814d271f76f5
Mark Roseman <mark@markroseman.com>
2019-05-25 17:28:59 -0700

    event bindings example

---
+++ bindings.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+l =ttk.Label(root, text="Starting...")
+l.grid()
+l.bind('<Enter>', lambda e: l.configure(text='Moved mouse inside'))
+l.bind('<Leave>', lambda e: l.configure(text='Moved mouse outside'))
+l.bind('<1>', lambda e: l.configure(text='Clicked left mouse button'))
+l.bind('<Double-1>', lambda e: l.configure(text='Double clicked'))
+l.bind('<B3-Motion>', lambda e: l.configure(text='right button drag to %d,%d' % (e.x, e.y)))
+root.mainloop()
