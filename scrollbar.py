9674be8e9b033c216eda87212f1e190d53c65673
Mark Roseman <mark@markroseman.com>
2019-05-25 17:32:19 -0700

    scrollbar example

---
+++ scrollbar.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+l = Listbox(root, height=5)
+l.grid(column=0, row=0, sticky=(N,W,E,S))
+s = ttk.Scrollbar(root, orient=VERTICAL, command=l.yview)
+s.grid(column=1, row=0, sticky=(N,S))
+l['yscrollcommand'] = s.set
+ttk.Sizegrip().grid(column=1, row=1, sticky=(S,E))
+root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(0, weight=1)
+for i in range(1,101):
+    l.insert('end', 'Line %d of 100' % i)
+root.mainloop()
