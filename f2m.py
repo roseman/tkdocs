0c871ffb41e9349bd8edcb595e646273870f5171
Mark Roseman <mark@markroseman.com>
2020-08-08 15:06:29 -0700

    fix calculation, end of line

--- f2m.py
+++ f2m.py
@@ -4,10 +4,10 @@ from tkinter import ttk
 def calculate(*args):
     try:
         value = float(feet.get())
-        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
     except ValueError:
         pass
-    
+
 root = Tk()
 root.title("Feet to Meters")
 
