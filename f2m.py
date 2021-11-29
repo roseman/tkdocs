8b139ca34debfd1f7ab68ef2a314064c030df038
Jonadabe <986504+Jonadabe@users.noreply.github.com>
2021-11-29 11:58:59 +0000

    Adding some code formating spaces

--- f2m.py
+++ f2m.py
@@ -1,12 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 
+
 def calculate(*args):
     try:
         value = float(feet.get())
         meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
     except ValueError:
         pass
+
 
 root = Tk()
 root.title("Feet to Meters")
